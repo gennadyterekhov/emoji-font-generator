af87cfffb9b13e3b41d3bac9896e02c6afd45ce1
Gennady Terekhov <ter.ge@yandex.ru>
2026-02-28 11:05:54 +0300

    emojis

--- cmd/create_svgs.py
+++ cmd/create_svgs.py
@@ -1,3 +1,27 @@
-'''
-create an svg file for every word in ai_output.json
-'''
+"""
+create an SVG file for every word in ai_output.json
+"""
+from pathlib import Path
+
+from lirbantu.combine import combine4
+from lirbantu.project import read_json_file
+
+
+
+def main():
+    words=read_json_file(Path("ai_output.json"))
+
+    for w in words:
+        try:
+            combine4(w['wordform'],[
+                w['root1_emoji'],
+                w['root2_emoji'],
+                w['logic'],
+                w['grammar'],
+            ])
+        except Exception as e:
+            print(e)
+
+
+if __name__ == "__main__":
+    main()
