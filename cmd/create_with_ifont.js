3f5d4b4adce4f7302550912d551f14a2cc1ccd1d
Gennady Terekhov <ter.ge@yandex.ru>
2026-02-28 15:08:33 +0300

    emojis

--- cmd/create_with_ifont.js
+++ cmd/create_with_ifont.js
@@ -1,14 +1,55 @@
 import ifont from 'ifont';
 import fs from 'node:fs';
 
-// Generate a TTF font, as a Uint8Array, from some SVG icons
+function readSvgFileSync(filePath) {
+  try {
+    return fs.readFileSync(filePath, 'utf8');
 
-const ttf = ifont({
-  icons: [
-    {name: 'circle', content: '<svg>...</svg>'},
-    {name: 'square', content: '<svg>...</svg>'},
-    {name: 'triangle', content: '<svg>...</svg>'},
-  ]
-});
+  } catch (error) {
+    throw new Error(`Error reading JSON file: ${error.message}`);
+  }
+}
 
-fs.writeFileSync('ifont.ttf', ttf);
+function readJsonFileSync(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(data);
+  } catch (error) {
+    throw new Error(`Error reading JSON file: ${error.message}`);
+  }
+}
+
+function getAiDictionary() {
+  return readJsonFileSync('config/ai_output.json')
+}
+
+function getIfontOptions(dict) {
+  const options = {
+    icons: []
+  }
+
+  for (let i; i < dict.length; i++) {
+    const wordformData = dict[i]
+    const svgPath = `emojis/combined/${wordformData['wordform']}.svg`
+    const svgContent = readSvgFileSync(svgPath)
+    options.icons.push({name: wordformData['wordform'], content: svgContent});
+  }
+
+  return options
+}
+
+function main() {
+  const dict = getAiDictionary()
+  const options = getIfontOptions(dict)
+  // const ttf = ifont({
+  //   icons: [
+  //     {name: 'circle', content: '<svg>...</svg>'},
+  //     {name: 'square', content: '<svg>...</svg>'},
+  //     {name: 'triangle', content: '<svg>...</svg>'},
+  //   ]
+  // });
+  const ttf = ifont(options);
+  fs.writeFileSync('ifont.ttf', ttf);
+}
+
+main()
