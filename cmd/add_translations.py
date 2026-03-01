78615c9fe60cbd4e06c4ee5173a385175e107cd7
Gennady Terekhov <ter.ge@yandex.ru>
2026-03-01 11:04:35 +0300

    emojis

---
+++ cmd/add_translations.py
@@ -0,0 +1,16 @@
+"""
+this is a temporary script to add lirbantu words to ai_output.json so that we can use translated words for the font ligatures
+"""
+from lirbantu.project import read_json_file, get_project_dir, write_json_file
+
+
+def main():
+    rootdir = get_project_dir()
+    pth = f'{rootdir}/config/ai_output.json'
+    before = read_json_file(pth)
+    after = {}
+    write_json_file(pth, after)
+
+
+if __name__ == "__main__":
+    main()
