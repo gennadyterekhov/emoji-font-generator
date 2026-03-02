8688268e9b265401fd8b5c3268e08033c208af36
Gennady Terekhov <ter.ge@yandex.ru>
2026-03-02 10:25:18 +0300

    refacto

--- cmd/add_emojis_to_dictionary.py
+++ cmd/add_emojis_to_dictionary.py
@@ -1,67 +1,10 @@
-import json
 import os
 import time
-from typing import Optional
 
 from dotenv import load_dotenv
 
 from emoji_font_generator.input.config import get_dictionary, save_new_dictionary
-from emoji_font_generator.llm.llm import ask_ai, get_prompt_for_one_word
-
-
-def add_emojis_to_one_word(api_key: str, model_name: str, base_url: str, word: dict) -> Optional[dict]:
-    data_for_prompt = {
-        "word": word['natural'],
-        "logic": [
-            "and",
-            "or",
-            "not",
-            "genitive",
-            "accusative",
-            "instrumental"
-        ],
-        "grammar": [
-            "adv",
-            "adj",
-            "n",
-            "plural",
-            "v",
-            "imperative",
-            "pr part",
-            "past part",
-            "diminutive",
-            "augmentative",
-            "causative"
-        ]
-    }
-    prompt = get_prompt_for_one_word(data_for_prompt)
-    try:
-        ai_response = ask_ai(api_key, model_name, base_url, prompt)
-        if 'content' in ai_response:
-            md_json = ai_response['content']
-            structured_response = md_json.replace('```json', '')
-            structured_response = structured_response.replace('```', '')
-            structured_response = json.loads(structured_response)
-        else:
-            print('unexpected response', ai_response)
-            return None
-
-        if 'error' in structured_response:
-            print(f'error classifying word {word['conlang']}={word['natural']}, error: {structured_response["error"]}')
-            return None
-        word['root1'] = structured_response.get('root1', '')
-        word['root2'] = structured_response.get('root2', '')
-        word['logic'] = structured_response.get('logic', '')
-        word['grammar'] = structured_response.get('grammar', '')
-        word['root1_emoji'] = structured_response.get('root1_emoji', '')
-        word['root2_emoji'] = structured_response.get('root2_emoji', '')
-        word['description'] = structured_response.get('description', '')
-
-    except Exception as e:
-        print(f'could not decode ai response for word {word['conlang']}={word['natural']}, error: {e}')
-        return None
-
-    return word
+from emoji_font_generator.llm.llm import add_emojis_to_one_word
 
 
 def add_emojis_to_dictionary(api_key: str, model_name: str, base_url: str) -> None:
--- src/emoji_font_generator/llm/llm.py
+++ src/emoji_font_generator/llm/llm.py
@@ -1,4 +1,5 @@
 import json
+from typing import Optional
 
 import requests
 
@@ -6,7 +7,7 @@ from emoji_font_generator.input.io import read_md_file
 from emoji_font_generator.project import get_project_dir
 
 
-def ask_ai(api_key: str, model_name: str, base_url: str, prompt: str) -> str|dict:
+def ask_ai(api_key: str, model_name: str, base_url: str, prompt: str) -> str | dict:
     response = requests.post(
         url=base_url,
         headers={
@@ -37,3 +38,61 @@ def get_prompt_for_one_word(input_data: dict) -> str:
     raw_prompt = read_md_file(path)
 
     return raw_prompt.replace('{{{input_data}}}', json.dumps(input_data))
+
+
+def add_emojis_to_one_word(api_key: str, model_name: str, base_url: str, word: dict) -> Optional[dict]:
+    data_for_prompt = {
+        "word": word['natural'],
+        "logic": [
+            "and",
+            "or",
+            "not",
+            "genitive",
+            "accusative",
+            "instrumental"
+        ],
+        "grammar": [
+            "adv",
+            "adj",
+            "n",
+            "plural",
+            "v",
+            "imperative",
+            "pr part",
+            "past part",
+            "diminutive",
+            "augmentative",
+            "causative"
+        ]
+    }
+    prompt = get_prompt_for_one_word(data_for_prompt)
+    ai_response = ''
+    try:
+        ai_response = ask_ai(api_key, model_name, base_url, prompt)
+        if 'content' in ai_response:
+            md_json = ai_response['content']
+            structured_response = md_json.replace('```json', '')
+            structured_response = structured_response.replace('```', '')
+            structured_response = json.loads(structured_response)
+        else:
+            print('unexpected response', ai_response)
+            return None
+
+        if 'error' in structured_response:
+            print(f'error classifying word {word['conlang']}={word['natural']}, error: {structured_response["error"]}')
+            return None
+        word['root1'] = structured_response.get('root1', '')
+        word['root2'] = structured_response.get('root2', '')
+        word['logic'] = structured_response.get('logic', '')
+        word['grammar'] = structured_response.get('grammar', '')
+        word['root1_emoji'] = structured_response.get('root1_emoji', '')
+        word['root2_emoji'] = structured_response.get('root2_emoji', '')
+        word['description'] = structured_response.get('description', '')
+
+    except Exception as e:
+        print(f'could not decode ai response for word {word['conlang']}={word['natural']}')
+        print(f'error: {e}')
+        print(f'ai_response: {ai_response}')
+        return None
+
+    return word
