d788587161acc994188449a7bb66c9bf464596a8
Gennady Terekhov <ter.ge@yandex.ru>
2026-03-02 10:17:05 +0300

    refacto

--- cmd/create_combined_svgs.py
+++ cmd/create_combined_svgs.py
@@ -1,22 +1,23 @@
 """
-create an SVG file for every word in ai_output.json
+create an SVG file for every word in dictionary.json
 """
 from pathlib import Path
 
 from emoji_font_generator.combine import  combine_wordform
-from emoji_font_generator.project import read_json_file, get_project_dir
+from emoji_font_generator.input.config import get_dictionary
+from emoji_font_generator.project import  get_project_dir
 
 
 def main():
     root = get_project_dir()
-    words = read_json_file(f"{root}/config/ai_output.json")
+    words = get_dictionary()
     prefix = f'{root}/emojis/combined'
     failures = 0
     for i, w in enumerate(words):
+        natural_word = w["natural"]
         try:
-            print(f'processing {w["wordform"]}, {i}/{len(words)}')
-            wordform = w["wordform"]
-            if Path(f'{prefix}/{wordform}.svg').exists():
+            print(f'processing {w["conlang"]}={natural_word}, {i}/{len(words)}')
+            if Path(f'{prefix}/{natural_word}.svg').exists():
                 continue
             combine_wordform(w)
         except Exception as e:
