6cd432036d7b9060d527d74665d20975e2e5a8b8
Gennady Terekhov <ter.ge@yandex.ru>
2026-03-02 11:02:00 +0300

    refacto

--- src/emoji_font_generator/input/io.py
+++ src/emoji_font_generator/input/io.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 def write_json_file(path: Path | str, data) -> None:
     with open(path, "w") as f:
-        json.dump(data, f, indent=4, ensure_ascii=False)
+        json.dump(data, f, indent=8, ensure_ascii=False)
 
 
 def read_json_file(path: Path | str):
