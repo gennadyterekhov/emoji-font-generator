9c15ea00e91914b51ccd94405c6c738ae7232b8b
Gennady Terekhov <ter.ge@yandex.ru>
2026-03-02 10:20:03 +0300

    refacto

--- cmd/download_all_emoji_svgs_from_twemoji.py
+++ cmd/download_all_emoji_svgs_from_twemoji.py
@@ -1,14 +1,16 @@
-from emoji_font_generator.config.config import  get_emojis_used_by_ai
 from emoji_font_generator.download import download_svg_from_twemoji
+from emoji_font_generator.input.config import get_emojis_used_by_ai
 
 
 def download_list(emojis):
-    failures=0
+    failures = 0
     for emoji in emojis:
-        success = download_svg_from_twemoji(emoji)
-        if not success:
-            failures+=1
+        if emoji:
+            success = download_svg_from_twemoji(emoji)
+            if not success:
+                failures += 1
     print(f'failures {failures}')
+
 
 def main():
     download_list(get_emojis_used_by_ai())
